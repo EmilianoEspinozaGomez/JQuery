e824a7f91a510e2438e261bbf333d0f05a74a01e
EmilianoEspinozaGomez <129553330+EmilianoEspinozaGomez@users.noreply.github.com>
2023-05-12 21:58:26 -0500

    Add files via upload

---
+++ archivo_js.js
@@ -0,0 +1,22 @@
+function validarNuevoUsuario(){
+	let response = confirm("Seguro que desea enviar?");
+
+	if (response) { // Caso presiona Aceptar
+                /* Obtener un campo */
+                let txtNombre = $("#txtNombre").value;
+                let txtEmail = $("#txtEmail").value;
+                /* Validar el valor del campo nombre */
+                if (txtNombre.length > 50 || txtNombre.length < 2) {
+                    alert("Su nombre no es aceptado");
+                    return false;
+                }
+                /* Validar el valor del campo correo */
+                if (txtEmail.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) == null) {
+                    alert("Correo incorrecto");
+                    return false;
+                }
+            } else { // Caso presiona Cancelar
+                return false;
+            }
+	
+};
